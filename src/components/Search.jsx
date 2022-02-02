a59b89d05409e708a864473b47bd1a359238cbc8
al_dente0 <al_dente0@whatever.com>
2022-02-02 17:07:51 +0300

    layout quantity search results

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,72 +1,76 @@
 import React from "react"
-
 export class Search extends React.Component  {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      search: '',
-      filter: 'all'
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			search: '',
+			filter: 'all',
+			pageNumber: 1,
+		}
+	}
 
-  hanldeKeyDown = (e) => {
-    if (e.code === 'Enter') {
-      e.preventDefault();
-      this.props.getSearchValue(this.state.search, this.state.filter);
-    }
-  }
+	hanldeKeyDown = (e) => {
+		if (e.code === 'Enter') {
+			e.preventDefault();
+			this.props.getSearchValue(this.state.search, this.state.filter);
+		}
+	}
 
-  handleClick = () => {
-    this.props.getSearchValue(this.state.search, this.state.filter)
-  }
-  
-  handleFilter = (e) => {
-    this.setState(() => ({filter: e.target.dataset.type}), () => {
-      this.props.getSearchValue(this.state.search, this.state.filter);
-    })
-  }
+	handleClick = () => {
+		this.props.getSearchValue(this.state.search, this.state.filter)
+	}
+	
+	handleFilter = (e) => {
+		this.setState(() => ({filter: e.target.dataset.type,}), () => {
+			this.props.getSearchValue(this.state.search, this.state.filter);
+		})
+	}
 
-  hanldeChange = (e) => {
-    this.setState({[e.target.name]: e.target.value})
-  }
+	
 
-  render() {
-    return <div className="row">
-      <div className="input-field">
-        <input 
-          id="search"
-          type="search"
-          name="search"
-          className="validate"
-          placeholder="Search"
-          onChange={this.hanldeChange}
-          onKeyDown={this.hanldeKeyDown}
-      />
-       
-      <button className="btn search-btn #1a237e indigo darken-4 right" onClick={this.handleClick}>search</button>
-    </div>
-    <div className="radio-btns left">
-      <p>
-      <label>
-          <input className="with-gap" name="filter"  type="radio" data-type='all' onChange={this.handleFilter} checked={this.state.filter === 'all'}/>
-          <span>All</span>
-        </label>
-      </p>
-      <p>
-        <label>
-          <input className="with-gap" name="filter" type="radio" data-type='movie' onChange={this.handleFilter} checked={this.state.filter === 'movie'}/>
-          <span>Movies only</span>
-        </label>
-      </p>
-      <p>
-        <label>
-          <input className="with-gap" name="filter"type="radio" data-type='series' onChange={this.handleFilter} checked={this.state.filter === 'series'}/>
-          <span>Series only</span>
-        </label>
-      </p>
-      </div>
-  </div>
-  }
-    
+	hanldeChange = (e) => {
+		this.setState({[e.target.name]: e.target.value})
+	}
+
+	render() {
+
+		return <div className="row">
+			<div className="input-field">
+				<input 
+					id="search"
+					type="search"
+					name="search"
+					className="validate"
+					placeholder="Search"
+					onChange={this.hanldeChange}
+					onKeyDown={this.hanldeKeyDown}
+			/>
+			 
+			<button className="btn search-btn #1a237e indigo darken-4 right" onClick={this.handleClick}>search</button>
+		</div>
+		<div className="radio-btns left">
+			<p>
+			<label>
+					<input className="with-gap" name="filter"  type="radio" data-type='all' onChange={this.handleFilter} checked={this.state.filter === 'all'}/>
+					<span>All</span>
+				</label>
+			</p>
+			<p>
+				<label>
+					<input className="with-gap" name="filter" type="radio" data-type='movie' onChange={this.handleFilter} checked={this.state.filter === 'movie'}/>
+					<span>Movies only</span>
+				</label>
+			</p>
+			<p>
+				<label>
+					<input className="with-gap" name="filter"type="radio" data-type='series' onChange={this.handleFilter} checked={this.state.filter === 'series'}/>
+					<span>Series only</span>
+				</label>
+			</p>
+			</div>
+			<div className="totalResults right">Matches found: {this.props.totalResults}</div>
+	</div>
+	}
+		
 }
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -10,22 +10,34 @@ export class Main extends React.Component {
     state = {
         movies: [],
         loading: true,
+        totalResults: 0,
+        searchValue: '',
+        type: '',
     }
 
-    getSearchValue = (searchValue, type) => {
+    getSearchValue = (searchValue = this.state.searchValue, type, pageNumber = 1) => {
         this.setState({loading: true});
         if (type === 'all') {
             fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchValue}`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false}))
+            .then(data => this.setState({movies: data.Search,
+                totalResults: Number(data.totalResults),
+                loading: false,
+                searchValue,
+            }))
             .catch((err) => {
                 console.error(err);
                 this.setState({loading: false});
             })
         } else {
-            fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchValue}&type=${type}`)
+            fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchValue}&type=${type}&page=${pageNumber}`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false}))
+            .then(data =>  this.setState({movies: data.Search,
+                totalResults: Number(data.totalResults),
+                loading: false,
+                searchValue,
+                type,
+            }))
             .catch((err) => {
                 console.error(err);
                 this.setState({loading: false});
@@ -38,7 +50,11 @@ export class Main extends React.Component {
 
         fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=avenger`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false}))
+            .then(data => this.setState({movies: data.Search,
+                totalResults: Number(data.totalResults),
+                loading: false,
+                searchValue: 'avenger',
+            }))
             .catch((err) => {
                 console.error(err);
                 this.setState({loading: false});
@@ -46,12 +62,12 @@ export class Main extends React.Component {
     }
     
     render() {
-        const {movies, loading} = this.state;
+        const {movies, loading, totalResults} = this.state;
 
         return (
             <main className="content">
                 <div className="container">
-                <Search getSearchValue={this.getSearchValue}/>
+                <Search getSearchValue={this.getSearchValue} totalResults={totalResults}/>
                 {
                     loading ? ( 
                         <Preloader/>
